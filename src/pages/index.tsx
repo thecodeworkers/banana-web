2bfd1383e7ffea717b4a24256b4a3b0eb85627bb
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-10-05 18:56:57 -0400

    init package

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+
+const HomePage = () => {
+  return (
+   
+      <div>
+        <p>BANANA CREATIVE </p>
+      </div>
+   
+  )
+}
+
+// export const getServerSideProps = wrapper.getServerSideProps(
+//   ({ store }) => store.dispatch(getResources())
+// )
+
+export default HomePage
