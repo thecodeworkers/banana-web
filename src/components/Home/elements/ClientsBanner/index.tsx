432f09f12453d344bdcd6e9d4147eb5194c60276
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-11-04 11:04:59 -0400

    fix indent

--- src/components/Home/elements/ClientsBanner/index.tsx
+++ src/components/Home/elements/ClientsBanner/index.tsx
@@ -5,20 +5,20 @@ import { fallbackRestUrl } from '../../../../utils/path'
 const ClientsBanner = (content) => {
 
   return (
-      <div className={styles._bannerContainer}>
-        <div className={styles._container}>
-          <div className={styles._content}>
-            {content?.content?.Clients.map((item, index) => {
-              return (
-                <div className={styles._logosContainer} key={index}>
-                  <Image src={`${fallbackRestUrl}${item?.logo?.url}`} alt={item?.name} width={100} height={150} quality={100} />
-                </div>
-              )
-            })
-            }
-          </div>
+    <div className={styles._bannerContainer}>
+      <div className={styles._container}>
+        <div className={styles._content}>
+          {content?.content?.Clients.map((item, index) => {
+            return (
+              <div className={styles._logosContainer} key={index}>
+                <Image src={`${fallbackRestUrl}${item?.logo?.url}`} alt={item?.name} width={100} height={150} quality={100} />
+              </div>
+            )
+          })
+          }
         </div>
       </div>
+    </div>
   )
 }
 
