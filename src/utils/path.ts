dbd9c1bdb433e6b1ce22ca08ccc66c58cb1bfdbe
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-11-22 20:06:30 -0400

    fix redirect videos2

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,7 +1,7 @@
-// export const fallbackUrl = 'https://banana-dev-api.thecodeworkers.com/graphql'
-// export const fallbackRestUrl: string = 'https://banana-dev-api.thecodeworkers.com'
+export const fallbackUrl = 'https://banana-dev-api.thecodeworkers.com/graphql'
+export const fallbackRestUrl: string = 'https://banana-dev-api.thecodeworkers.com'
 
-export const fallbackUrl = 'http://localhost:1337/graphql'
-export const fallbackRestUrl: string = 'http://localhost:1337'
+// export const fallbackUrl = 'http://localhost:1337/graphql'
+// export const fallbackRestUrl: string = 'http://localhost:1337'
 
 export default fallbackUrl
