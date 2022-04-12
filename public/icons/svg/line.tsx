2f3ed95fe6d8e49056bfafa737c8827012d61471
MauriceMiot <maurice.yggdrasil@gmail.com>
2022-04-12 18:19:23 -0400

    fixed line

--- public/icons/svg/line.tsx
+++ public/icons/svg/line.tsx
@@ -1,13 +1,14 @@
 const Line = () => (
-  <svg width="100%" height="5" viewBox="0 0 294 4" fill="none" >
-    <path d="M294 0H0V3H294V0Z" fill="url(#paint0_linear_2372_137)" />
+  <svg width="100%" height="2.3" viewBox="0 0 500 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M500 0H0V3H500V0Z" fill="url(#paint0_linear_2372_136)" />
     <defs>
-      <linearGradient id="paint0_linear_2372_137" x1="0" y1="1.50022" x2="294" y2="1.50022" gradientUnits="userSpaceOnUse">
-        <stop stopColor="#FF70A0" />
+      <linearGradient id="paint0_linear_2372_136" x1="0" y1="1.50022" x2="500" y2="1.50022" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FF70A0" />
         <stop offset="1" stopColor="#DF2833" />
       </linearGradient>
     </defs>
   </svg>
+
 )
 
 export default Line
