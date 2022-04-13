c2de1556331943cd4853334fe5c6adb42c86dec7
MauriceMiot <maurice.yggdrasil@gmail.com>
2022-04-12 20:21:09 -0400

    fixed line 2

--- public/icons/svg/line.tsx
+++ public/icons/svg/line.tsx
@@ -1,5 +1,5 @@
 const Line = () => (
-  <svg width="100%" height="2.3" viewBox="0 0 500 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="100%" height="2.38" viewBox="0 0 500 3" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M500 0H0V3H500V0Z" fill="url(#paint0_linear_2372_136)" />
     <defs>
       <linearGradient id="paint0_linear_2372_136" x1="0" y1="1.50022" x2="500" y2="1.50022" gradientUnits="userSpaceOnUse">
