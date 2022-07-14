e492adf10d14cb78df006bb052ec697ed4ab3029
decode9 <jorgebastidas9@gmail.com>
2022-07-14 12:25:03 -0400

    update path

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,5 +1,5 @@
-export const fallbackUrl = 'https://banana-stg-api.thecodeworkers.com/graphql'
-export const fallbackRestUrl: string = 'https://banana-stg-api.thecodeworkers.com'
+export const fallbackUrl = 'https://admin.bananacreative.io/graphql'
+export const fallbackRestUrl: string = 'https://admin.bananacreative.io'
 
 // export const fallbackUrl = 'http://localhost:1337/graphql'
 // export const fallbackRestUrl: string = 'http://localhost:1337'
